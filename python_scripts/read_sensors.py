1b771ad737dec5755d18d7de76e85c65e5e12d37
Eric Kuha <erickuha@gmail.com>
2016-01-16 17:47:26 -0600

    Splitting python scripts in preparation for setting up CRON jobs

--- python_scripts/read_sensors.py
+++ python_scripts/read_sensors.py
@@ -1,3 +1,4 @@
+#!/user/bin/python2.7
 """
 file: agriduino.py
 python version: 2.7
@@ -5,7 +6,6 @@ description:
     This script polls an arduino for info and builds at as a comma delimited data string to write
     to a csv file.  
     String should be formatted "datestamp, temp, humidity, light_level, soil_moisture, watering
-
 """
 
 import time, datetime
@@ -13,55 +13,38 @@ import csv
 import schedule
 import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+#Constants
+CSV_FILE = '/home/pi/agridata/agri_data.csv'
+ARDUINO_ADDRESS = '/dev/ttyACM0'
+BAUD = 9600
 
-def poll_sensors():
+def poll_sensors(ser):
     """
     Read from sensors
     """
     print("Polling sensors\n")
     ser.write('1')
-    time.sleep(1)
+    time.sleep(2)
+    print("reading data")
     data = ser.readline()
     data = data.strip()
     data = time.strftime("%c") + "," + data
-    out = data.split(',')
-    print(out)
+    print(data)
     print("\n")
-    send_to_file(out)
+    send_to_file(data.split(','))
 
 
 def send_to_file(data):
     """
     Write to CSV file at path
     """
-    CSV_FILE = '/home/pi/agridata/agri_data.csv'
     with open(CSV_FILE, 'a') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for line in data:
-            writer.writerow(line)
+        writer.writerow(data)
         csv_file.close()
 
-def water_plants():
-    """
-    Set flag on Arduino to water plants
-    """
-    print("Water Plants")
-
-
-def __main__():
-	"""
-	Main function and scheduler
-	"""
-	time.sleep(10) #wait for conneciton to arduino
-	
-    #State
-    schedule.every(1).minutes.do(poll_sensors)
-    schedule.every().day.at("7:00").do(water_plants)
-
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-__main__()
+# Connect to Arduino.  This has the unfortunate side effect of causing the
+# duino to reset. Will fix in hardware.
+ser = serial.Serial(ARDUINO_ADDRESS, BAUD)
+time.sleep(2)
+poll_sensors(ser)
